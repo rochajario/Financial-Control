282328089be5282e5d312fee169615846971c3a3
Jario Rocha <rochajario@gmail.com>
2022-01-23 13:42:47 -0300

    update on deploy script

--- modules/frontend/deploy.sh
+++ modules/frontend/deploy.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env sh
 set -e
+cd modules/frontend/
+npm install
 npm run build
 cd dist
 git init
 git add - A 
 git commit -m 'deploy'
-git push -f git@github.com:rochajario/Financial-Control.git frontend:gh-pages
+git push -f git@github.com:rochajario/Financial-Control.git main:gh-pages
 cd - 
